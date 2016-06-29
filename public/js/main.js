e903b74153ac069e39938b679cde87efac067265
Chris DeMoll <demoll.chris@gmail.com>
2016-06-29 11:54:20 -0400

    able to add and remove from day 1

--- public/js/main.js
+++ public/js/main.js
@@ -46,18 +46,68 @@ $(function initializeMap (){
     activity: '/images/star-3.png'
   };
 
-  function drawMarker (type, coords) {
+  var markers = {};
+
+  function drawMarker (type, coords, name) {
     var latLng = new google.maps.LatLng(coords[0], coords[1]);
     var iconURL = iconURLs[type];
     var marker = new google.maps.Marker({
       icon: iconURL,
       position: latLng
     });
+    markers[name] = marker;
     marker.setMap(currentMap);
   }
 
-  drawMarker('hotel', [40.705137, -74.007624]);
-  drawMarker('restaurant', [40.705137, -74.013940]);
-  drawMarker('activity', [40.716291, -73.995315]);
+  $('#hotel-choices').siblings('button').on('click', function() {
+    var selectedHotel = $(this).siblings('select').val();
+    $('#itinerary div:nth-child(1) ul').append('<div class="itinerary-item"><span class="title">' + selectedHotel + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    drawMarker('hotel', findHotelCoords(selectedHotel), selectedHotel);
+  });
+
+
+
+  function findHotelCoords(name) {
+    for (var i=0; i<hotels.length; i++) {
+      if (hotels[i].name === name) {
+        return hotels[i].place.location;
+      }
+    }
+  }
+
+  $('#restaurant-choices').siblings('button').on('click', function() {
+    var selectedRestaurant = $(this).siblings('select').val();
+    $('#itinerary div:nth-child(2) ul').append('<div class="itinerary-item"><span class="title">' + selectedRestaurant + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    drawMarker('restaurant', findRestaurantCoords(selectedRestaurant), selectedRestaurant);
+  });
+
+  function findRestaurantCoords(name) {
+    for (var i=0; i<restaurants.length; i++) {
+      if (restaurants[i].name === name) {
+        return restaurants[i].place.location;
+      }
+    }
+  }
+
+  $('#activity-choices').siblings('button').on('click', function() {
+    var selectedActivity = $(this).siblings('select').val();
+    $('#itinerary div:nth-child(3) ul').append('<div class="itinerary-item"><span class="title">' + selectedActivity + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    drawMarker('activity', findActivityCoords(selectedActivity), selectedActivity);
+    });
+
+  function findActivityCoords(name) {
+    for (var i=0; i<activities.length; i++) {
+      if (activities[i].name === name) {
+        return activities[i].place.location;
+      }
+    }
+  }
+
+  $('#itinerary .list-group').on('click', 'button', function() {
+    var name = $(this).siblings('span').text();
+    $(this).parent('div').remove();
+    markers[name].setMap(null);
+
+  })
 
 });
